711186e5b244c66f74a38dc34996a5adc819de51
ct1559 <e.cooper.taylor@gmail.com>
2022-07-08 10:28:52 -0400

    Fix issues with feedback
    Feedback was giving extra W's when guess
    contained multiple of the same digit when
    code only contained a single instance of
    the digit.

--- computer.rb
+++ computer.rb
@@ -3,14 +3,15 @@ class Computer
   
   def initialize
     @score = 0
-    @code = computer_code
+    @code = create_code
+    @set = guess_set
   end
 
   def update_score(num)
     @score += num
   end
 
-  def computer_code
+  def create_code
     code = []
     for i in 0..3
       code.push(rand(1..6))
@@ -18,13 +19,49 @@ class Computer
     code
   end
 
+  def guess_code(turn, feedback="")
+    if turn == 1
+      1122
+    else
+      puts 'in progress'
+      puts guess_set
+    end
+  end
+
+  def guess_set
+    set = []
+    for i in 1..6
+      for j in 1..6
+        for k in 1..6
+          for l in 1..6
+            set.push(i.to_s + j.to_s + k.to_s + l.to_s)
+          end
+        end
+      end
+    end
+    set
+  end
+
   def give_feedback(guess, code)
     feedback = []
+    # Count how many time each digit occurs in code
+    digit_count = Hash.new(0)
+    code.each {|digit| digit_count[digit] += 1}
+    p digit_count
     guess.each_with_index do |num, index|
+      p digit_count
       if code.include?(num)
         if guess[index] == code[index]
-          feedback.push('R')
-        else
+          if digit_count[num] == 0
+            feedback.delete_at(feedback.index('W'))
+            feedback.push('R')
+          else
+            digit_count[num] -= 1
+            feedback.push('R')
+          end
+        elsif digit_count[num] > 0 && guess[index] != code[index]
+          puts "In elsif: #{digit_count[num]}"
+          digit_count[num] -= 1
           feedback.push('W')
         end
       end
